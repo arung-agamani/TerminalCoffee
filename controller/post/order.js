c1d3c5ed18404aa2bd9bd8add388f0cc6000e94e
unknown <arung.agamani@gmail.com>
2019-08-09 21:12:01 +0700

    Proper order logic finished

--- controller/post/order.js
+++ controller/post/order.js
@@ -1,4 +1,34 @@
+const Queue = require('../../model/QueueModel');
+
 module.exports = (req, res) => {
-    console.log(req.body);
-    res.redirect('/order');
+    const OrderArray = new Array();
+    const itemArray = new Array();
+    const amountArray = new Array();
+    
+    for (var prop in req.body) {
+        if (prop.match(/item/)) {
+            itemArray.push(req.body[prop]);
+        } else {
+            amountArray.push(req.body[prop]);
+        }
+    }
+
+    for (let i = 0; i < itemArray.length; i++) {
+        let obj = {
+            name : itemArray[i],
+            amount : amountArray[i],
+        };
+        OrderArray.push(obj);
+    }
+    Queue.create({
+        username : req.session.username,
+        order : OrderArray,
+    }, (err, queueRes) => {
+        if (err) {
+            console.log('Error occured when pushing queue order : ', err);
+            return res.redirect('/order');
+        }
+        console.log('Successfully adding new order!');
+        res.redirect('/');
+    });
 }
